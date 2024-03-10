788a0e2f430695cef487f5cc824b779b007fe7dd
Renan <109163037+rznan@users.noreply.github.com>
2024-03-10 14:45:04 -0300

    Add resolução do exercício 3

---
+++ Listas/l3Processos/src/ex03/view/Main.java
@@ -0,0 +1,10 @@
+package ex03.view;
+
+import ex03.controller.DistroController;
+
+public class Main {
+    public static void main(String[] args) {
+        DistroController dc = new DistroController();
+        dc.showDistro();
+    }
+}
