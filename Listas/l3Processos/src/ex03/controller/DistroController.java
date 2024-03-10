b53d68580901442bf901a430ad9abb1564d61e4f
Renan <109163037+rznan@users.noreply.github.com>
2024-03-10 15:12:06 -0300

    fix exercício 3

--- Listas/l3Processos/src/ex03/controller/DistroController.java
+++ Listas/l3Processos/src/ex03/controller/DistroController.java
@@ -9,31 +9,32 @@ public class DistroController {
         return System.getProperty("os.name");
     }
 
-    private void readProcess(String[] cmd){
-        try {
-            Process process = Runtime.getRuntime().exec(cmd);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            if((line = reader.readLine()) != null){
-                while (line != null){
-                    System.out.println(line);
-                    line = reader.readLine();
-                }
-            }
-            else{
-                reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-                while ((line = reader.readLine()) != null){
-                    System.err.println(line);
-                }
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
     public void showDistro(){
         if(os().contains("inux")) {
-            readProcess(new String[] {"cat", "/etc/os-release"});
+            String[] cmd = {"cat", "/etc/os-release"};
+            try {
+                Process process = Runtime.getRuntime().exec(cmd);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                if((line = reader.readLine()) != null){
+                    while (line != null){
+                        if(line.startsWith("NAME=")){
+                            System.out.print(
+                                line.substring(line.indexOf("\""), line.length()-1)
+                                + " ");
+                        }
+                        else if (line.startsWith("VERSION=")) {
+                            System.out.println(
+                                    line.substring(line.indexOf("\"", line.length()-1))
+                            );
+                        }
+
+                        line = reader.readLine();
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
         } else {
             System.err.println("O Seu sistema operacional não é do tipo Unix");
         }
