e4bc104d1ceb58bf569ea13b127c7c45ce48a209
Renan <109163037+rznan@users.noreply.github.com>
2024-04-16 14:18:49 -0300

    add solução do exercício 1 da lista 10 de revisão >> add dependências com estruturas de dados e algoritmos de ordenação

---
+++ Listas/l10RevisaoP1/src/ex01/controller/ProcessThread.java
@@ -0,0 +1,42 @@
+package ex01.controller;
+
+import java.util.concurrent.Semaphore;
+
+public class ProcessThread extends Thread {
+    int id;
+    String name;
+    int SecRuntime;
+    Semaphore executionSemaphore;
+
+    public ProcessThread(int id, String name, Semaphore executionSemaphore) {
+        this.id = id;
+        this.name = name;
+        this.SecRuntime = calculateRuntime();
+        this.executionSemaphore = executionSemaphore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            executionSemaphore.acquire();
+            System.out.println(this);
+            sleep(SecRuntime * 100L);
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            executionSemaphore.release();
+        }
+    }
+
+    private int calculateRuntime() {
+        return ((int) (Math.random() * 117) + 4);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[id: %02d | nome: %-15s | tempo execução: %03d seg.]",
+                this.id,
+                this.name,
+                this.SecRuntime);
+    }
+}
---
+++ Listas/l10RevisaoP1/src/ex01/view/Main.java
@@ -0,0 +1,57 @@
+package ex01.view;
+
+import datastructures.genericQueue.Queue;
+import ex01.controller.ProcessThread;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class Main {
+    private static final String processNamesPath = "Listas/l10RevisaoP1/nomesDeProcessos.txt";
+    public static void main(String[] args) {
+        Queue<String> namesQueue = readProcessesName(processNamesPath);
+        Semaphore Executionsemaphore = new Semaphore(1);
+        ProcessThread[] processes = createProcesses(namesQueue, Executionsemaphore);
+        runScheduller(processes);
+    }
+
+    private static void runScheduller(ProcessThread[] processes) {
+        for(ProcessThread p : processes) {
+            p.start();
+        }
+    }
+
+    private static ProcessThread[] createProcesses(Queue<String> namesQueue, Semaphore executionsemaphore) {
+        int cont = 0;
+        ProcessThread[] processThreads = new ProcessThread[namesQueue.size()];
+        while(!namesQueue.isEmpty()) {
+            String name;
+            try {
+                name = namesQueue.remove();
+            } catch (Exception e) {
+                name = "UNNAMED";
+            }
+            processThreads[cont]= new ProcessThread(cont, name, executionsemaphore);
+            cont++;
+        }
+        return processThreads;
+    }
+
+    public static Queue<String> readProcessesName(String fileName) {
+        File file = new File(fileName);
+        Queue<String> names = new Queue<>();
+        try{
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                names.insert(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            for(int i = 1; i < 21; i++) {
+                names.insert("Processo" + (i));
+            }
+        }
+        return names;
+    }
+}
