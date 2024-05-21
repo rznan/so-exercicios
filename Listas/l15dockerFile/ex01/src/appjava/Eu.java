c027b0946f9bc57b9768b5d31686a431b0b7bb13
rznan <sc.renan7@gmail.com>
2024-05-21 17:49:24 -0300

    lista 1 de dockerfile
    ex 1 incompleto

---
+++ Listas/l15dockerFile/ex01/src/appjava/Eu.java
@@ -0,0 +1,12 @@
+public class Eu {
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("RENAN SANTOS CARVALHO");
+			try {
+				Thread.sleep(300000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
